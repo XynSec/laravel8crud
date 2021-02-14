da4690dd1755611c203b7df6bd4c9a3226b91640
Rahma Syndu Grananta <timsurga@gmail.com>
2021-02-14 20:13:30 +0700

    nice

---
+++ resources/views/template.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tutorial CRUD Laravel 8 untuk Pemula - xynsec.my.id</title>
+   <!-- Buat Manggil Bootstrapnya -->
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Buat Manggil Bootstrapnya -->
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+
+<div class="container">
+    @yield('content')
+</div>
+
+</body>
+</html>
